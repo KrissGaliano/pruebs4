97cd042871f8dd4484265e91238b3f105f31ee05
Kriss Galiano Orbezo <kgaliano@ebizlatin.com>
2023-06-05 11:25:44 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<h2>Redirect to a Webpage</h2>
+<p>The replace() method replaces the current document with a new one:</p>
+
+<button onclick="myFunction()">Replace document</button>
+
+<script>
+function myFunction() {
+  location.replace("https://www.ebiz.pe/")
+}
+</script>
+
+</body>
+</html> 
+
